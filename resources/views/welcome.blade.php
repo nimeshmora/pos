1e344e579871fabd5b6293134f744ee03487fcc2
nimeshmora <mhnimesh@gmail.com>
2017-06-26 05:12:11 +0530

    pos cloud fifth-tst5 commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -72,7 +72,7 @@
                         <a href="{{ url('/home') }}">Home</a>
                     @else
                         <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Nimesha hellos</a>
+                        <a href="{{ url('/register') }}">Nimesha hellossdlfkj</a>
                     @endif
                 </div>
             @endif
